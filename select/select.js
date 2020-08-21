9f229cb0680cde4ddac0a0dcc1b191d42b36c1c3
Alexey197 <margadonnn3@gmail.com>
2020-08-21 16:46:12 +0300

    first commit

---
+++ select/select.js
@@ -0,0 +1,41 @@
+const getTemplate = () => {
+  return `
+    <div class="select__input">
+      <span>Text</span>
+      <i class="fa fa-chevron-down"></i>
+    </div>
+    <div class="select__dropdown">
+      <ul class="select__list">
+        <li class="select__item">1</li>
+        <li class="select__item">2</li>
+        <li class="select__item">3</li>
+        <li class="select__item">4</li>
+        <li class="select__item">5</li>
+        <li class="select__item">6</li>
+        <li class="select__item">7</li>
+        <li class="select__item">8</li>
+      </ul>
+    </div>
+  `
+}
+
+export class Select {
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector)
+    
+    this.#render()
+  }
+  
+  #render() {
+    this.$el.classList.add('select')
+    this.$el.innerHTML = getTemplate()
+  }
+  
+  open() {
+    this.$el.classList.add('open')
+  }
+  
+  close() {
+    this.$el.classList.remove('open')
+  }
+}
